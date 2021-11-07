ce995c08d28f5b400ea4d889ad9e37170fe69954
“Yehor <egromad27@gmail.com>
2021-11-07 17:39:03 +0200

    Make separate search/my_words screens

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:just_audio/just_audio.dart';
-import 'package:my_voc/secrets.dart';
+import 'package:my_voc/screens/my_words_screen.dart';
+import 'package:my_voc/screens/search_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,80 +8,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  TextEditingController _wordController = TextEditingController();
-  AudioPlayer player = AudioPlayer();
+  List<Widget> _screens = [SearchScreen(), MyWordsScreen()];
 
-  String _definition = '';
-  String _pronunciation = '';
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  TextButton(
-                    onPressed: () async {
-                      if (_pronunciation != null) {
-                        var duration = await player.setUrl(_pronunciation);
-                        player.play();
-                      }
-                    },
-                    child: Icon(
-                      Icons.volume_up_rounded,
-                    ),
-                  ),
-                  Expanded(
-                    child: TextField(
-                      controller: _wordController,
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      var url =
-                          'https://od-api.oxforddictionaries.com/api/v2/entries/en/${_wordController.text}';
-
-                      var response = await http.get(Uri.parse(url), headers: {
-                        'app_id': kAppId,
-                        'app_key': kAppKey,
-                      });
-
-                      if (response.statusCode == 200) {
-                        var decodedResponse = jsonDecode(response.body);
-                        try {
-                          var definition = decodedResponse['results'][0]
-                                  ['lexicalEntries'][0]['entries'][0]['senses']
-                              [0]['definitions'][0];
-
-                          _pronunciation = decodedResponse['results'][0]
-                                  ['lexicalEntries'][0]['entries'][0]
-                              ['pronunciations'][0]['audioFile'];
-
-                          setState(() {
-                            _definition = definition;
-                          });
-                        } catch (e) {
-                          print(e);
-                        }
-                      }
-                    },
-                    child: Icon(Icons.send),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 50.0,
-              ),
-              Text(_definition),
-            ],
-          ),
-        ),
-      ),
+      body: SafeArea(child: _screens[_selectedIndex]),
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
@@ -93,10 +25,19 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.list_alt),
-            label: 'Words',
-          )
+            label: 'My Words',
+          ),
         ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
       ),
     );
+  }
+
+  void _onItemTapped(int value) {
+    setState(() {
+      _selectedIndex = value;
+    });
   }
 }
---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,85 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:http/http.dart' as http;
+import 'package:my_voc/secrets.dart';
+
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController _wordController = TextEditingController();
+  AudioPlayer player = AudioPlayer();
+
+  String _definition = '';
+  String _pronunciation = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              TextButton(
+                onPressed: () async {
+                  if (_pronunciation != null) {
+                    var duration = await player.setUrl(_pronunciation);
+                    player.play();
+                  }
+                },
+                child: Icon(
+                  Icons.volume_up_rounded,
+                ),
+              ),
+              Expanded(
+                child: TextField(
+                  controller: _wordController,
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  var url =
+                      'https://od-api.oxforddictionaries.com/api/v2/entries/en/${_wordController.text}';
+
+                  var response = await http.get(Uri.parse(url), headers: {
+                    'app_id': kAppId,
+                    'app_key': kAppKey,
+                  });
+
+                  if (response.statusCode == 200) {
+                    var decodedResponse = jsonDecode(response.body);
+                    try {
+                      var definition = decodedResponse['results'][0]
+                              ['lexicalEntries'][0]['entries'][0]['senses'][0]
+                          ['definitions'][0];
+
+                      _pronunciation = decodedResponse['results'][0]
+                              ['lexicalEntries'][0]['entries'][0]
+                          ['pronunciations'][0]['audioFile'];
+
+                      setState(() {
+                        _definition = definition;
+                      });
+                    } catch (e) {
+                      print(e);
+                    }
+                  }
+                },
+                child: Icon(Icons.send),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+          Text(_definition),
+        ],
+      ),
+    );
+  }
+}
